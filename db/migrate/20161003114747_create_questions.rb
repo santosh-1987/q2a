77d5152523c09f149372f6d7280f9603d3dd788b
Santosh Kumar Mohanty <santoshkumar.mohanty@timeinc.com>
2016-10-03 18:11:25 +0530

    Added Controllers for tag / Question / Users

---
+++ db/migrate/20161003114747_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :questions do |t|
+      t.string :question, null: false, unique: true ,limit:1000
+      t.references :user, foreign_key: true
+      t.references :tag, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
